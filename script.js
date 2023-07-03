28a9c359e6fb3405ad3793e431495a91da657bc0
vikash <vikash.tomar.india@gmail.com>
2023-07-04 00:00:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// URL   "https://dummyjson.com/products"
+
+
+
+function somesh(){
+    fetch("https://dummyjson.com/products")
+    .then((res)=>{ 
+         return res.json()
+
+    })
+        
+            
+        .then(result=>{
+            show(result.products)
+            console.log(result);
+        })
+}
+somesh()
+
+function show(data){
+    for(let i= 0; i<data.length; i++)
+    {
+        let div= document.createElement("div");
+        div.classList.add("products");
+        let img = document.createElement("img");
+        let anchor = document.createElement("a");
+
+        anchor.href = "singleproducts?.html?id="+data[i].id;
+        
+        anchor.append(img)
+        img.src = data[i].thumbnail;
+        let h3 = document.createElement("h3");
+
+        let title = document.createElement("title");
+        title.innerHTML = data[i].title;
+        let description = document.createElement("p");
+        description.innerHTML = data[i].description;
+        let price = document.createElement("p");
+        price.innerHTML = data[i].price;
+
+    div.append(anchor);
+    div.append(h3);
+    div.append(title);
+    div.append(description);
+    div.append(price)
+
+
+
+    document.querySelector("#products").append(div)
+
+    }
+
+    
+
+}
+
+
---
+++ singlep.js
@@ -0,0 +1,50 @@
+const pageurl = window.location.href
+console.log(pageurl)
+
+
+if(!pageurl.includes("id")){
+
+}
+else{
+    const id = pageurl.split("=")[1]
+    console.log(id)
+    see(id)
+}
+
+
+async function see(id){
+    const response = await fetch("https://dummyjson.com/products/"+id)
+    const result = response.json()
+    console.log(result)
+    show(result)
+}
+
+
+function show(result){
+    let div = document.createElement("div");
+    div.classList.add("products");
+    let img = document.createElement("img");
+    img.src = result.thumbnails;
+    let h3 = document.createElement("h3");
+    let price = document.createElement("p");
+    price.innerHTML = result.price
+    let des = document.createElement("p");
+    des.innerHTML = result.description;
+    let addtoCart = document.createElement("a");
+    addtoCart.innerHTML = "AddToCart"
+
+
+    div.append(img)
+    div.append(h3)
+    div.append(price)
+    div.append(des)
+    div.append(addtoCart)
+
+    let a = document.querySelector("#singleproduct");
+
+
+    a.append(div)
+
+
+
+}
